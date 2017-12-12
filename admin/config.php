a0db13601bb9471ab6e07814ec429617554477d3
gambitok <gambitokgd@gmail.com>
2017-12-12 16:17:59 +0200

    uploaded opencart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart2/admin/');
+define('HTTP_CATALOG', 'http://opencart2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart2/admin/');
+define('HTTPS_CATALOG', 'http://opencart2/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/domains/opencart2/admin/');
+define('DIR_SYSTEM', 'E:/OpenServer/domains/opencart2/system/');
+define('DIR_IMAGE', 'E:/OpenServer/domains/opencart2/image/');
+define('DIR_LANGUAGE', 'E:/OpenServer/domains/opencart2/admin/language/');
+define('DIR_TEMPLATE', 'E:/OpenServer/domains/opencart2/admin/view/template/');
+define('DIR_CONFIG', 'E:/OpenServer/domains/opencart2/system/config/');
+define('DIR_CACHE', 'E:/OpenServer/domains/opencart2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/OpenServer/domains/opencart2/system/storage/download/');
+define('DIR_LOGS', 'E:/OpenServer/domains/opencart2/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/OpenServer/domains/opencart2/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/OpenServer/domains/opencart2/system/storage/upload/');
+define('DIR_CATALOG', 'E:/OpenServer/domains/opencart2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
