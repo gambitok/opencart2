33b48d23d3428bc484fd11cfdb450beb0743a609
gambitok <gambitokgd@gmail.com>
2017-12-14 10:38:22 +0200

    added controller Datev

--- catalog/controller/common/datev.php
+++ catalog/controller/common/datev.php
@@ -1,10 +1,11 @@
 <?php
+//Controller Datev
 class ControllerCommonDatev extends Controller {
 	public function index() {
 		$this->load->language('common/datev');
 
 		$data['datev'] = $this->language->get('datev');
-
+		
 		return $this->load->view('common/datev', $data);
 	}
 }
